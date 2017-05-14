4502e740affbefe28a15b3b967de6a97d2ec5c01
Teodor <teodor@bedford.st>
2017-05-14 12:47:02 +0200

    hjälpfunktioner i main

--- databas/queries.sql
+++ databas/queries.sql
@@ -3,3 +3,12 @@ from specin
 join team
 on specin.teamID = team.teamID
 where issue = {}
+
+
+
+
+select * from patient
+join inqueue
+on inqueue.patid = patient.pnum
+where teamid = %s
+order by teamid, prio;
